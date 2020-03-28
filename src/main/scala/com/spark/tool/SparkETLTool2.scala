34106662acb14df2703867318adbcc612c61530d
chengwang2019 <chengwang611@gmail.com>
2020-03-28 19:00:35 -0400

    fix function

--- src/main/scala/com/spark/tool/SparkETLTool2.scala
+++ src/main/scala/com/spark/tool/SparkETLTool2.scala
@@ -117,11 +117,8 @@ object Aggregation2 {
     return null
 
   }
-}
-
-
-
- def readfile4(path: String, spark: SparkSession): DataFrame = {
+   
+   def readfile4(path: String, spark: SparkSession): DataFrame = {
     if (path.endsWith("csv")) {
       return spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(path)
 
@@ -147,5 +144,11 @@ object Aggregation2 {
     
     return null
 
-  }
+  } 
+   
+}
+
+
+
+
 // scalastyle:on println
