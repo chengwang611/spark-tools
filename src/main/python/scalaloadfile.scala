7e18ffdb0bd8e8469fca5b153b5d8b395ab694fe
chengwang611@gmail <chengwang611@gmail>
2024-11-18 20:51:23 +0000

    test note edit

--- src/main/python/scalaloadfile.scala
+++ src/main/python/scalaloadfile.scala
@@ -9,3 +9,21 @@ val pathVolume = s"/Volumes/$catalog/$schema/$volume"
 val pathTable = s"$catalog.$schema"
 println(pathVolume) // Show the complete path
 println(pathTable) // Show the complete path
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC here i will do the dataframe
+
+// COMMAND ----------
+
+// COMMAND ----------
+
+dbutils.fs.cp(downloadUrl, s"$pathVolume/$fileName")
+val data = Seq((2021, "test", "Albany", "M", 42))
+val columns = Seq("Year", "First_Name", "County", "Sex", "Count")
+
+val df1 = data.toDF(columns: _*)
+
+display(df1) // The display() method is specific to Databricks notebooks and provides a richer visualization.
+// df1.show() The show() method is a part of the Apache Spark DataFrame API and provides basic visualization.
